fe7c598db1e716f339264c288ba51635fde264cc
httpnotonly <httpnotonly@gmail.com>
2020-10-27 21:12:15 +0400

    + ports

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Got 403? Try to bypass it with XFF headers
 
-`docker run --rm --name forbid-bypasser -e VULN_ID=1 -e DOMAIN=site.com whitespots/forbid-bypasser`
+`docker run --rm --name forbid-bypasser -e VULN_ID=1 -e PORTS=443,8080 -e DOMAIN=site.com whitespots/forbid-bypasser`
--- check.py
+++ check.py
@@ -4,11 +4,18 @@ import json
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-url = 'https://{0}/'.format(os.environ.get('DOMAIN'))
+ports = os.environ.get('PORTS')
+urls = ['https://{0}/'.format(os.environ.get('DOMAIN'))]
+try:
+    ports = ports.strip(' ').split(',')
+    for port in ports:
+        urls.append('http://{0}:{1}/'.format(os.environ.get('DOMAIN'), port))
+except:
+    pass
 vuln_id = os.environ.get('VULN_ID')
 
 
-def resp(state=False):
+def resp(state=False, url=urls[0]):
     if state:
         return json.dumps({"vulnerable": "True", "vuln_id": vuln_id, "description": url})
     else:
@@ -16,29 +23,31 @@ def resp(state=False):
 
 
 def check():
-    try:
-        dirty_response = requests.get(url, timeout=4, verify=False)
-        if dirty_response.status_code == 403 or \
-                '403' in dirty_response.content or \
-                'forbidden' in dirty_response.content.lower():
-            response = requests.get(
-                url,
-                verify=False,
-                headers={
-                    'X-Forwarded-For': '127.0.0.1',
-                    'X-Forwarded-Host': '127.0.0.1',
-                    'X-Real-IP': '127.0.0.1',
-                    'X-Client-IP': '127.0.0.1',
-                    # according to https://tools.ietf.org/html/rfc7239#section-7.4
-                    'Forwarded': 'for=127.0.0.1;host=127.0.0.1'
-                },
-            )
-            if response.status_code != 403 and \
-                    '403' not in response.content and \
-                    'forbidden' not in response.content.lower():
-                return resp(True)
-    except:
-        pass
+    for url in urls:
+        try:
+            dirty_response = requests.get(url, timeout=4, verify=False)
+            if dirty_response.status_code == 403 or \
+                    '403' in dirty_response.content or \
+                    'forbidden' in dirty_response.content.lower():
+                response = requests.get(
+                    url,
+                    verify=False,
+                    headers={
+                        'X-Forwarded-For': '127.0.0.1',
+                        'X-Forwarded-Host': '127.0.0.1',
+                        'X-Real-IP': '127.0.0.1',
+                        'X-Client-IP': '127.0.0.1',
+                        # according to https://tools.ietf.org/html/rfc7239#section-7.4
+                        'Forwarded': 'for=127.0.0.1;host=127.0.0.1'
+                    },
+                    timeout=4
+                )
+                if response.status_code != 403 and \
+                        '403' not in response.content and \
+                        'forbidden' not in response.content.lower():
+                    return resp(True, url)
+        except:
+            pass
     return resp(False)
 
 
